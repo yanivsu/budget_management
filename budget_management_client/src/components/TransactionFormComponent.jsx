b599b70356c06cc9e2b266705f8b54f0a7fb7ae2
ronksks <ron.yalensky@gmail.com>
2024-04-02 18:56:01 +0300

    added support for the Form to act both as update and submit form, with changing buttons, also in update mode al inputs have the transaction details in them.

--- budget_management_client/src/components/TransactionFormComponent.jsx
+++ budget_management_client/src/components/TransactionFormComponent.jsx
@@ -1,12 +1,22 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
-import { Container, Button, Form, Row } from "react-bootstrap";
-import { addTransaction } from "../actions/transactionAction";
+import { Container, Button, Form, Row, FormLabel } from "react-bootstrap";
+import {
+  addTransaction,
+  updateTransaction,
+} from "../actions/transactionAction";
 import { useDispatch } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import Spinner from "react-bootstrap/Spinner";
 
-export default function TransactionFormComponent() {
+export default function TransactionFormComponent({
+  transaction: initialTransaction = null,
+}) {
+  const location = useLocation();
+  // use transaction object from the state for Update or an initial empty transaction for Add
+  const transaction = location.state?.transaction || initialTransaction;
+  const isUpdating = Boolean(transaction); // true if updating, false if creating
+
   let navigate = useNavigate();
 
   const dispatch = useDispatch();
@@ -17,7 +27,20 @@ export default function TransactionFormComponent() {
     handleSubmit,
     formState: { errors },
     reset,
+    setValue,
   } = useForm();
+  useEffect(() => {
+    if (transaction) {
+      setValue("transaction_id", transaction.transaction_id);
+      setValue("transaction_name", transaction.transaction_name);
+      setValue("transaction_details", transaction.transaction_details);
+      setValue("amount", transaction.amount);
+      const date = new Date(transaction.date);
+      const formattedDate = date.toISOString().split("T")[0];
+      setValue("date", formattedDate);
+      setValue("type", transaction.type);
+    }
+  }, [transaction, setValue]);
 
   const onSubmit = (data) => {
     setLoading(true);
@@ -26,7 +49,17 @@ export default function TransactionFormComponent() {
     setTimeout(() => {
       navigate("/table");
       setLoading(false);
-    }, 1500); // 5000ms delay
+    }, 1500); // 1500ms delay
+  };
+  // TODO when submiting update, form refresh and nothing happens
+  const updateHandler = (data) => {
+    setLoading(true);
+    console.log(data);
+    dispatch(updateTransaction(data));
+    setTimeout(() => {
+      navigate("/table");
+      setLoading(false);
+    }, 1500); // 1500ms delay
   };
 
   const clearForm = () => reset();
@@ -38,10 +71,11 @@ export default function TransactionFormComponent() {
       style={{ minHeight: "100vh" }}
     >
       <Row className="mb-3">
-        <h2>Add a Transaction</h2>
+        <h2>{isUpdating ? "Update a Transaction" : "Add a Transaction"}</h2>{" "}
       </Row>
       <Form
-        onSubmit={handleSubmit(onSubmit)}
+        // onSubmit={handleSubmit(onSubmit)}
+        onSubmit={isUpdating ? updateHandler : handleSubmit(onSubmit)}
         style={{ width: "100%", maxWidth: "400px" }}
       >
         <Form.Group controlId="transaction_name">
@@ -50,6 +84,18 @@ export default function TransactionFormComponent() {
             type="text"
             {...register("transaction_name", { required: true })}
             isInvalid={errors.transactionName}
+          />
+          <Form.Control.Feedback type="invalid">
+            This field is required
+          </Form.Control.Feedback>
+        </Form.Group>
+
+        <Form.Group controlId="transaction_details">
+          <FormLabel>Transaction Details:</FormLabel>
+          <Form.Control
+            type="text"
+            {...register("transaction_details", { required: true })}
+            isInvalid={errors.transactiondetails}
           />
           <Form.Control.Feedback type="invalid">
             This field is required
@@ -79,11 +125,12 @@ export default function TransactionFormComponent() {
             This field is required
           </Form.Control.Feedback>
         </Form.Group>
+
         {/*TODO add arrow dropdown*/}
         <Form.Group controlId="type">
           <Form.Label>Type:</Form.Label>
           <Form.Control as="select" {...register("type", { required: true })}>
-            <option value="">Select...</option>
+            <option value="">Select Transaction Type...</option>
             <option value="income">Income</option>
             <option value="expense">Expense</option>
           </Form.Control>
@@ -91,8 +138,23 @@ export default function TransactionFormComponent() {
             This field is required
           </Form.Control.Feedback>
         </Form.Group>
-        <div className="d-flex justify-content-center">
-          <Button className="mt-2 " variant="primary" size="sm" type="submit">
+        <div className="d-flex justify-content-center mt-2">
+          <Button
+            className=""
+            variant="outline-primary"
+            size="sm"
+            type="button"
+            // onClick={navigate("/table")}
+            onClick={goBack}
+          >
+            Go back
+          </Button>
+          <Button
+            className="mx-2 "
+            variant="outline-primary"
+            size="sm"
+            type="submit"
+          >
             {loading ? (
               <Spinner
                 className="me-2"
@@ -102,28 +164,20 @@ export default function TransactionFormComponent() {
                 role="status"
                 aria-hidden="true"
               />
+            ) : isUpdating ? (
+              "Update"
             ) : (
               "Submit"
             )}
           </Button>
           <Button
-            className="mt-2 mx-2 "
-            variant="primary"
+            className=" "
+            variant="outline-primary"
             size="sm"
             type="button"
             onClick={clearForm}
           >
             Clear
-          </Button>
-          <Button
-            className="mt-2 "
-            variant="primary"
-            size="sm"
-            type="button"
-            // onClick={navigate("/table")}
-            onClick={goBack}
-          >
-            Go back
           </Button>
         </div>
       </Form>
